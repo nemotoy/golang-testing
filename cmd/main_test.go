e8d22b3f3d8cff6dfc4b1cbbcf42a7584d34e5d9
yuma-nemoto <yuma-nemoto@dac.co.jp>
2019-08-06 23:08:50 +0900

    Add an unit test with testify

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSumWithReflect(t *testing.T) {
@@ -13,4 +15,13 @@ func TestSumWithReflect(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Fatal()
 	}
+}
+
+func TestSumWithTestify(t *testing.T) {
+	a := assert.New(t)
+	i := 1
+	i2 := 2
+	want := 3
+	got := Sum(i, i2)
+	a.EqualValues(want, got)
 }
