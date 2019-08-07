ddf3ce875aea332a9cb7374fd2de6c9eea4e9929
yuma-nemoto <yuma-nemoto@dac.co.jp>
2019-08-08 08:20:26 +0900

    Remove unused unit tests

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -9,15 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSumWithReflect(t *testing.T) {
-	i, i2 := 1, 2
-	want := 3
-	got := Sum(i, i2)
-	if !reflect.DeepEqual(got, want) {
-		t.Fatal()
-	}
-}
-
 func BenchmarkSumWithReflect(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		i, i2 := i, i+1
@@ -29,14 +20,6 @@ func BenchmarkSumWithReflect(b *testing.B) {
 	}
 }
 
-func TestSumWithTestify(t *testing.T) {
-	a := assert.New(t)
-	i, i2 := 1, 2
-	want := 3
-	got := Sum(i, i2)
-	a.EqualValues(want, got)
-}
-
 func BenchmarkSumWithTestify(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		a := assert.New(b)
@@ -44,15 +27,6 @@ func BenchmarkSumWithTestify(b *testing.B) {
 		want := i + i2
 		got := Sum(i, i2)
 		a.EqualValues(want, got)
-	}
-}
-
-func TestSumWithCmp(t *testing.T) {
-	i, i2 := 1, 2
-	want := 3
-	got := Sum(i, i2)
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Errorf("Sum() mismatch (-want +got):\n%s", diff)
 	}
 }
 
@@ -67,14 +41,6 @@ func BenchmarkSumWithCmp(b *testing.B) {
 	}
 }
 
-func TestSumWithIs(t *testing.T) {
-	is := is.New(t)
-	i, i2 := 1, 2
-	want := 3
-	got := Sum(i, i2)
-	is.Equal(want, got)
-}
-
 func BenchmarkSumWithIs(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		is := is.New(b)
@@ -82,13 +48,5 @@ func BenchmarkSumWithIs(b *testing.B) {
 		want := i + i2
 		got := Sum(i, i2)
 		is.Equal(want, got)
-	}
-}
-
-func TestObjectWithCmp(t *testing.T) {
-	want := Object{"a", 1, true}
-	got := genObject("a", 1, true)
-	if !cmp.Equal(want, got) {
-		t.Error("genObject() doesn't equal")
 	}
 }
