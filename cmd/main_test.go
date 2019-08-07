e7beccaa7e7d79ad988ecc7e50ec928c1d01833b
yuma-nemoto <yuma-nemoto@dac.co.jp>
2019-08-07 13:05:03 +0900

    Add benchmark functions

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -18,12 +18,33 @@ func TestSumWithReflect(t *testing.T) {
 	}
 }
 
+func BenchmarkSumWithReflect(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		i, i2 := i, i+1
+		want := i + i2
+		got := Sum(i, i2)
+		if !reflect.DeepEqual(got, want) {
+			b.Fatal()
+		}
+	}
+}
+
 func TestSumWithTestify(t *testing.T) {
 	a := assert.New(t)
 	i, i2 := 1, 2
 	want := 3
 	got := Sum(i, i2)
 	a.EqualValues(want, got)
+}
+
+func BenchmarkSumWithTestify(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		a := assert.New(b)
+		i, i2 := i, i+1
+		want := i + i2
+		got := Sum(i, i2)
+		a.EqualValues(want, got)
+	}
 }
 
 func TestSumWithCmp(t *testing.T) {
@@ -35,12 +56,33 @@ func TestSumWithCmp(t *testing.T) {
 	}
 }
 
+func BenchmarkSumWithCmp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		i, i2 := i, i+1
+		want := i + i2
+		got := Sum(i, i2)
+		if diff := cmp.Diff(want, got); diff != "" {
+			b.Errorf("Sum() mismatch (-want +got):\n%s", diff)
+		}
+	}
+}
+
 func TestSumWithIs(t *testing.T) {
 	is := is.New(t)
 	i, i2 := 1, 2
 	want := 3
 	got := Sum(i, i2)
 	is.Equal(want, got)
+}
+
+func BenchmarkSumWithIs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		is := is.New(b)
+		i, i2 := i, i+1
+		want := i + i2
+		got := Sum(i, i2)
+		is.Equal(want, got)
+	}
 }
 
 func TestObjectWithCmp(t *testing.T) {
